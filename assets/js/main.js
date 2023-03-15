7301b4774db9d3cec391a21d5c9a29bbeb78b761
mbillah.technoval <developer.technoval@gmail.com>
2023-03-15 12:08:16 +0600

    Added Client Review Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
   $('#hero-slider').owlCarousel({
     loop:true,
+    autoplay: true,
     items: 1,
     dots: false,
     nav: true,
     navText: ['PREV', 'NEXT'],
+    smartSpeed: 1000,
+  });
+
+  // Client Reviw
+  $('#client-review').owlCarousel({
+    loop:true,
+    autoplay: true,
+    items: 2,
+    dots: true,
+    nav: false,
     smartSpeed: 1000,
   })
 });
